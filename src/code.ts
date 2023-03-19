7ec6253c1b3abca56fae8296018e631a7c1d5850
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2023-03-20 00:14:13 +0900

    fix: get color tokens

--- src/code.ts
+++ src/code.ts
@@ -1,18 +1,15 @@
 figma.showUI(__html__)
 
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = []
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 1 } }]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
-    }
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+  if (msg.type === 'generate-tokens') {
+    const style = figma.getLocalPaintStyles()
+    const result = style.map((val) => val.name)
+
+    // カラートークンをログに出力する
+    console.log(result)
   }
 
-  // figma.closePlugin()
+  if (msg.type === 'cancel') {
+    figma.closePlugin()
+  }
 }
--- src/components/Sample.svelte
+++ src/components/Sample.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   let count: number = 1
-  const createObject = () => {
+  const generateTokens = () => {
     parent.postMessage(
-      { pluginMessage: { type: 'create-rectangles', count } },
+      { pluginMessage: { type: 'generate-tokens', count } },
       '*'
     )
   }
@@ -13,19 +13,11 @@
 </script>
 
 <div class="flex flex-col gap-4 p-4">
-  <p>
-    Count: <input
-      bind:value={count}
-      type="number"
-      class="w-20 rounded-sm border border-slate-500 px-2 py-1"
-    />
-  </p>
-
   <div class="flex gap-2">
     <button
       class="rounded-md bg-blue-500 py-1 px-2 font-normal text-blue-50"
       type="button"
-      on:click={createObject}>Create</button
+      on:click={generateTokens}>generate</button
     >
     <button
       class="rounded-md bg-slate-300 py-1 px-2 font-normal text-slate-800"
