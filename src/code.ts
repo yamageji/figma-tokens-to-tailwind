3722691249707afe582ae5669bfc7bcee4e358c4
K-shigehito <example@test.com>
2023-03-22 17:11:31 +0900

    fix: ui

--- src/code.ts
+++ src/code.ts
@@ -2,9 +2,8 @@ figma.showUI(__html__, { height: 600, width: 664 });
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === 'generate-tokens') {
-    const colorData = generateColorData();
+    const colorData = generateSemanticColorData(msg.state.prefix);
     const textData = generateTextData();
-
     figma.ui.postMessage({ colorData, textData });
   }
 };
@@ -12,24 +11,38 @@ figma.ui.onmessage = (msg) => {
 const rgbToHex = (r: number, g: number, b: number): string => {
   const componentToHex = (c: number): string => {
     const hex = Math.round(c * 255).toString(16);
-    return hex.length == 1 ? '0' + hex : hex;
+    return hex.length == 1 ? `0${hex}` : hex;
   };
-
   return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 };
 
-const generateColorData = (): string => {
-  const paintStyles = figma.getLocalPaintStyles();
-  const styleMap = {};
+const standardiseColours = (r: number, g: number, b: number): string => {
+  const standardization = (c: number): string => {
+    return Math.round(c * 255).toString();
+  };
+  return `${standardization(r)} ${standardization(g)} ${standardization(b)}`;
+};
 
+const generateSemanticColorData = (prefix: string): string => {
+  const paintStyles = figma.getLocalPaintStyles();
+
+  const styleMap = {};
   paintStyles.forEach((style) => {
     const paint = style.paints[0];
+    const description = style.description;
+
     if (paint.type === 'SOLID') {
       const styleNames = style.name.split('/');
       let currentStyle = styleMap;
       styleNames.forEach((name, index) => {
         if (!currentStyle[name]) {
           if (index === styleNames.length - 1) {
+            if (description) {
+              currentStyle[
+                name
+              ] = `rgb(var(--${prefix}-${description}) / <alpha-value>)`;
+              return;
+            }
             currentStyle[name] = rgbToHex(
               paint.color.r,
               paint.color.g,
@@ -45,6 +58,11 @@ const generateColorData = (): string => {
   });
 
   return JSON.stringify(styleMap, null, 2);
+};
+
+const generatePrimitiveColorData = (): string => {
+  const paintStyles = figma.getLocalPaintStyles();
+  return 'test';
 };
 
 const generateTextData = (): string => {
--- src/components/Sample.svelte
+++ src/components/Sample.svelte
@@ -4,12 +4,23 @@
   let colorData: string;
   let textData: string;
 
-  let addToExtendColor = false;
+  let prefix = 'primitive';
+  let hasPrimitiveColors = false;
   let classifyByType = false;
+  let addColorsToExtend = false;
+
   let addToExtendText = false;
 
   const generateTokens = () => {
-    parent.postMessage({ pluginMessage: { type: 'generate-tokens' } }, '*');
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: 'generate-tokens',
+          state: { prefix, hasPrimitiveColors },
+        },
+      },
+      '*'
+    );
   };
 
   onmessage = (event) => {
@@ -18,19 +29,34 @@
   };
 </script>
 
-<div class="flex gap-8 p-4">
-  <div class="flex min-h-[300px] min-w-[300px] flex-col gap-8">
+<div class="flex min-h-[300px] gap-8 p-4">
+  <div class="flex min-w-[200px] flex-col gap-8">
     <div class="flex flex-col gap-2">
       <h2 class="text-lg font-bold">Color Tokens</h2>
       <div class="flex flex-col gap-2">
         <label class="flex items-center gap-1">
-          <input type="checkbox" bind:checked={addToExtendColor} />
+          <input type="checkbox" bind:checked={hasPrimitiveColors} />
+          primitive colors
+        </label>
+
+        <label class="ml-5 flex items-center gap-2">
+          prefix:
+          <input
+            type="text"
+            bind:value={prefix}
+            class="w-full rounded-sm border border-slate-500 px-1 py-0.5"
+          />
+        </label>
+
+        <label class="flex items-center gap-1">
+          <input type="checkbox" bind:checked={addColorsToExtend} />
           add to extend
         </label>
+
         <div>
           <label class="flex items-center gap-1">
             <input type="checkbox" bind:checked={classifyByType} />
-            Classify by Type
+            classify by type
           </label>
           <ul class="mt-2 rounded-sm border border-slate-400 px-2 py-1 text-sm">
             <li>background color (bg/*)</li>
@@ -62,7 +88,7 @@
     <div
       class="flex h-fit w-full items-center justify-between rounded bg-slate-200 px-3 py-1 text-sm"
     >
-      tailwind.config
+      tailwind.config.js
       <span class="flex cursor-pointer gap-1">
         <IconClipboard />
         copy code
@@ -72,9 +98,31 @@
       class="flex-grow rounded border border-slate-400 px-2 py-1 font-mono text-sm leading-snug"
     >
       {#if colorData}
-        <pre>{colorData}</pre>
-        <pre>{textData}</pre>
+        <pre class="overflow-auto">
+          {colorData}
+        </pre>
       {/if}
     </div>
   </div>
+
+  {#if hasPrimitiveColors}
+    <div class="flex min-w-[300px] flex-col gap-2">
+      <div
+        class="flex h-fit w-full items-center justify-between rounded bg-slate-200 px-3 py-1 text-sm"
+      >
+        main.css
+        <span class="flex cursor-pointer gap-1">
+          <IconClipboard />
+          copy code
+        </span>
+      </div>
+      <div
+        class="flex-grow rounded border border-slate-400 px-2 py-1 font-mono text-sm leading-snug"
+      >
+        {#if colorData}
+          test
+        {/if}
+      </div>
+    </div>
+  {/if}
 </div>
